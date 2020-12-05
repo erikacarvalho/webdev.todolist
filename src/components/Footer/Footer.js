1072fe234a0ed27c08360b3f8b481898cb9c3c93
Erika Carvalho <erika.carvalho@protonmail.com>
2020-12-05 14:03:29 -0300

    add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <hr/>
+        <p id="footer-content">2020 todolist</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;

6ca2bb212056dfd995964280f07007112f637173
Erika Carvalho <erika.carvalho@protonmail.com>
2020-12-05 17:49:35 -0300

    add to-dos listing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,15 @@
 import './Home.css';
-import React from "react";
+import {React} from "react";
 import {Link} from "react-router-dom";
+import TodoList from "../TodoList/TodoList";
 
 function Home() {
   return (
-    <>
-      <main>
-        <h2>your minimalist to-do list</h2>
-        <p>to-do list goes here</p>
-        <p><Link to="/add">add new to-do link goes here</Link></p>
-      </main>
-    </>
+    <main>
+      <h2>your minimalist to-do list</h2>
+      <TodoList/>
+      <p><Link to="/add">add new to-do link goes here</Link></p>
+    </main>
   );
 }
 

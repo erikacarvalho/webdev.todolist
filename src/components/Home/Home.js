de2107ab358da54a11c1fbcfcdb53f568773965c
Erika Carvalho <erika.carvalho@protonmail.com>
2020-12-05 16:37:12 -0300

    add routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import './Home.css';
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Home() {
   return (
     <>
       <main>
+        <h2>your minimalist to-do list</h2>
         <p>to-do list goes here</p>
-        <p>add new to-do link goes here</p>
+        <p><Link to="/add">add new to-do link goes here</Link></p>
       </main>
     </>
   );

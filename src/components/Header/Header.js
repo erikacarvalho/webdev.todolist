18c52851cad647be2025072b47757adeb1636f0f
Erika Carvalho <erika.carvalho@protonmail.com>
2020-12-05 13:56:01 -0300

    add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>todolist</h1>
+        <hr/>
+      </header>
+    </>
+  );
+}
+
+export default Header;

14c6099bb9390239e91f689c01df5dd88d59b565
Erika Carvalho <erika.carvalho@protonmail.com>
2020-12-06 04:33:02 -0300

    add style sheets

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,12 @@
 import './Header.css';
 import {Link} from "react-router-dom";
+import React from "react";
 
 function Header() {
   return (
-    <>
       <header>
         <h1><Link to="/">todolist</Link></h1>
-        <hr/>
       </header>
-    </>
   );
 }
 
